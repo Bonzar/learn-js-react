6e9aa2de97d36d46e6ab3ccb5d69b092318c83df
Vladislav Navoyan <tickles.moment-0r@icloud.com>
2023-02-26 13:30:28 +0300

    done M4 practical-work

--- M4/src/shared/App.tsx
+++ M4/src/shared/App.tsx
@@ -1,11 +1,21 @@
 import { hot } from "react-hot-loader/root";
 import React from "react";
 
-import "the-new-css-reset/css/reset.css";
 import "./main.global.css";
+import { Layout } from "./Layout";
+import { Header } from "./Header";
+import { Content } from "./Content";
+import { CardsList } from "./CardsList";
 
 function AppComponent() {
-  return <div>content</div>;
+  return (
+    <Layout>
+      <Header />
+      <Content>
+        <CardsList />
+      </Content>
+    </Layout>
+  );
 }
 
 export const App = hot(AppComponent);

ffc5c9e55bb7cf6f8d59dcf85829f911636b6916
Vladislav Navoyan <tickles.moment-0r@icloud.com>
2023-03-01 11:06:16 +0300

    init M5 asset

---
+++ M5/bin/dev.js
@@ -0,0 +1,57 @@
+const webpack = require("webpack");
+const [
+  webpackClientConfig,
+  webpackServerConfig,
+] = require("../cfg/webpack.config");
+
+const webpackDevMiddleware = require("webpack-dev-middleware");
+const webpackHotMiddleware = require("webpack-hot-middleware");
+
+// Client
+const express = require("express");
+const nodemon = require("nodemon");
+const path = require("path");
+
+const hmrServer = express();
+const clientCompiler = webpack(webpackClientConfig);
+
+hmrServer.use(
+  webpackDevMiddleware(clientCompiler, {
+    publicPath: webpackClientConfig.output.publicPath,
+    serverSideRender: true,
+    writeToDisk: true,
+    stats: "errors-only",
+  })
+);
+
+hmrServer.use(
+  webpackHotMiddleware(clientCompiler, {
+    path: "/static/__webpack_hmr",
+  })
+);
+
+hmrServer.listen(3001, () => {
+  console.log("Hmr Server successfully started");
+});
+
+// Server
+compiler = webpack(webpackServerConfig);
+
+compiler.run((err) => {
+  if (err) {
+    console.error("Compilation failed: ", err);
+  }
+
+  compiler.watch({}, (err) => {
+    if (err) {
+      console.error("Compilation failed: ", err);
+    }
+    console.log("Compilation was successfully");
+  });
+
+  nodemon({
+    script: path.resolve(__dirname, "../dist/server/server.js"),
+    watch: path.resolve(__dirname, "../dist/server/"),
+    "delay": 1000
+  });
+});
---
+++ M5/src/server/server.js
@@ -0,0 +1,24 @@
+import express from "express";
+import ReactDOM from "react-dom/server";
+import { indexTemplate } from "./indexTemplate";
+import App from "../shared/App";
+
+const app = express();
+
+app.use("/static", express.static("./dist/client"));
+
+app.get("/", (req, res) => {
+  res.send(indexTemplate(ReactDOM.renderToString(<App />)));
+});
+
+app.get("/api", (req, res) => {
+  res.json({ message: "Hi, from server api." });
+});
+
+app.listen(3000, () => {
+  console.log("Server started on http://localhost:3000");
+});
+
+app.on('uncaughtException',  (err) => {
+  console.error('Caught exception: ', err);
+});

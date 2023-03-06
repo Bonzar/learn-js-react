8172f6dd6fc089f53aa0cb8d8bfae6d48c66c173
Vladislav Navoyan <tickles.moment-0r@icloud.com>
2023-03-06 21:19:07 +0300

    add js and react utils

---
+++ M5/src/utils/js/assignRandomId.ts
@@ -0,0 +1,7 @@
+import { assoc } from "./assoc";
+
+// it may be replaced with nanoid package
+export const generateRandomString = () => Math.random().toString(36).substring(2, 15);
+
+export const assignRandomId = <O extends object>(obj: O) =>
+  assoc("id", generateRandomString())(obj);

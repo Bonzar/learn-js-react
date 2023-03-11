14b22ad15b95cc8ac2817ff672d37878f22044e5
bonzar <nva112002@gmail.com>
2023-03-11 14:46:35 +0300

    added close the dropdown on outside click

--- M5/src/shared/components/UI/Dropdown/Dropdown.tsx
+++ M5/src/shared/components/UI/Dropdown/Dropdown.tsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styles from "./dropdown.css";
+import {stopPropagation} from "../../../../utils/react/stopPropagation";
 
 interface IDropdownProps {
   className?: string;
@@ -22,11 +23,25 @@ export function Dropdown({
 }: IDropdownProps) {
   const [isDropdownOpen, setIsDropdownOpen] = React.useState(isOpen);
 
+  useEffect(() => {
+    if (isDropdownOpen) {
+      document.body.addEventListener("click", handleBodyClick);
+    } else {
+      document.body.removeEventListener("click", handleBodyClick);
+    }
+
+    return () => document.body.removeEventListener("click", handleBodyClick);
+  }, [isDropdownOpen]);
+
   React.useEffect(() => setIsDropdownOpen(isOpen), [isOpen]);
   React.useEffect(
     () => (isDropdownOpen ? onOpen() : onClose()),
     [isDropdownOpen]
   );
+
+  const handleBodyClick = () => {
+    setIsDropdownOpen(false);
+  };
 
   const handleOpen = () => {
     if (isOpen === undefined) {
@@ -36,13 +51,16 @@ export function Dropdown({
 
   return (
     <div className={[styles.container, className].join(" ")}>
-      <div onClick={handleOpen}>{button}</div>
+      <div onClick={stopPropagation(handleOpen)}>{button}</div>
       {isDropdownOpen && (
         <div className={styles.listContainer}>
-          <div className={styles.list} onClick={(e) => {
-            e.stopPropagation();
-            setIsDropdownOpen(false)
-          }}>
+          <div
+            className={styles.list}
+            onClick={(e) => {
+              e.stopPropagation();
+              setIsDropdownOpen(false);
+            }}
+          >
             {children}
           </div>
         </div>
